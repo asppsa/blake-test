d2ef5366dacb970468c37060968d1d38266ffa76
Alastair Pharo <me@asph.dev>
2019-08-17 13:45:26 +1000

    Specs: add specs for Lesson and LessonPart models

--- spec/models/lesson_part_spec.rb
+++ spec/models/lesson_part_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe LessonPart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:lesson) { Lesson.create! number: 1 }
+
+  describe 'lesson' do
+    it 'validates the presence of a Lesson object' do
+      expect(LessonPart.create(number: 1)).not_to be_valid
+      expect(LessonPart.create(number: 1, lesson: lesson)).to be_valid
+    end
+  end
+
+  describe 'students' do
+    let(:students) { (0...5).map { Student.create! name: 'Jim' } }
+    subject { LessonPart.create! number: 1, lesson: lesson }
+
+    it 'is a list of students currently using this lesson part' do
+      expect do
+        students.each { |student| student.update! lesson_part: subject }
+      end
+        .to change { subject.reload.students }
+        .from(be_empty)
+        .to a_collection_containing_exactly(*students)
+    end
+
+    it "sets students' lesson parts to null if deleted" do
+      students.each { |student| student.update! lesson_part: subject }
+      subject.destroy
+      expect(students.map(&:reload).map(&:lesson_part)).to all be_nil
+    end
+  end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'parts' do
+    subject { described_class.create! number: 1 }
+
+    it 'is a one-to-many association with LessonPart' do
+      expect do
+        (1..3).each do |number|
+          LessonPart.create! number: number, lesson: subject
+        end
+      end
+        .to change { subject.reload.parts }
+        .from(be_empty)
+        .to(a_collection_containing_exactly(
+              an_object_matching(proc { |o| o.number == 1 }),
+              an_object_matching(proc { |o| o.number == 2 }),
+              an_object_matching(proc { |o| o.number == 3 })
+            ))
+    end
+
+    it 'always returns LessonParts in numbered order' do
+      unordered_numbers = [5, 7, 6, 4, 2, 1, 3]
+      unordered_numbers.each do |number|
+        LessonPart.create! number: number, lesson: subject
+      end
+
+      expect(LessonPart.order(:id).pluck(:number)).to eq unordered_numbers
+      expect(subject.parts.pluck(:number)).to eq unordered_numbers.sort
+    end
+  end
 end
